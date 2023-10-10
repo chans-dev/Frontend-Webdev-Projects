4386d26b32e1796f278f9daf3ebd30a2e255145a
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-10-10 23:32:55 +0530

    landing page updated

---
+++ basic-landing-page-with-routes/src/main.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+
+
+
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import Error from "./components/Error";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+      {
+        path: "/contact",
+        element: <ContactUs />,
+      },
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={appRouter} />
+  </React.StrictMode>
+);
